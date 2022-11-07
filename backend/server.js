52a0c9f2147ea31b21913a805a226adb6f1c6647
K M Amaz Uddin Shaon <k.m.shaonred@gmail.com>
2022-11-07 12:49:38 +0600

    first commit

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+// const cookieParser = require("cookie-parser");
+require("dotenv").config();
+
+// DataBase
+require("./db");
+
+// middleware
+
+// app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+var server = app.listen(8081, function () {
+  var port = server.address().port;
+
+  console.log(`Example app listening at http://localhost:${port}`);
+});
